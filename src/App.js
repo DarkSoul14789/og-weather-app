660ddc7a411d4d70cc5f92829b526f4b3a10180c
DarkSoul14789 <shubhrajivshah@gmail.com>
2022-04-02 16:44:29 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
+import axios from 'axios';
 import './App.css';
 
+const apiKey = "f9b33a08d72829195afab90e2e87ba7e";
+// const apiKey = "4babd81cab56b2491b8d0fdc5c244fc9";
+
+
+
 function App() {
+  const [data, setData] = useState({});
+  const [location, setLocation] = useState('');
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&appid=${apiKey}`;
+
+    const getData = (event)=>{
+      if(event.keyCode===13){
+        axios.get(url).then((response)=>{
+          // console.log(response);
+          setData(response.data);
+          // console.log(data);
+        }).catch((error)=>{
+          console.log(error.message);
+        })
+        setLocation('')
+      }
+    }
+    
+    
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      <div className="container">
+        <input type="text"  placeholder='City Name' onChange={(e)=>setLocation(e.target.value)} onKeyDown={getData} value={location}/>
+        <div className="upper">
+          <p className="name">{data.name ? data.name : null}</p>
+          <h1 className="temp">{data.main ? <h1>{data.main.temp.toFixed()} C</h1> : null}</h1>
+          <p className="weather">{data.weather ? data.weather[0].main : null}</p>
+        </div>
+        <div className="bottom">
+          <p className="feelsLike">Feels Like: {data.main ? <p>{data.main.feels_like.toFixed()} C</p> : null} </p>
+          <p className="tempMax">Max Temperature: {data.main ? <p>{data.main.temp_max.toFixed()} C</p> : null}</p>
+          <p className="tempMin">Min Temperature: {data.main ? <p>{data.main.temp_min.toFixed()} C</p> : null}</p>
+        </div>
+      </div>
     </div>
   );
 }
